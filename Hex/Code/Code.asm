8b49b42bb5d920f7d7d7285c4885191149142a9a
GabrielDelMonte <gabrieldmsn@hotmail.com>
2025-07-02 03:21:53 -0300

    Updated .hex files

--- Hex/Code/Code.asm
+++ Hex/Code/Code.asm
@@ -1,27 +1,29 @@
-     	lw $r1, grupo*0x0120+1($r31)              ;		r1 ← Mem[i + Número do Grupo * 0120,h]
-        lw $r2, grupo*0x0120+2($r31)              ;		r2 ← Mem[i + Número do Grupo * 0120,h]  
-        lw $r3, grupo*0x0120+3($r31)              ;		r3 ← Mem[i + Número do Grupo * 0120,h] 
-        lw $r4, grupo*0x0120+4($r31)              ;		r4 ← Mem[i + Número do Grupo * 0120,h] 
-        lw $r5, grupo*0x0120+5($r31)              ;		r5 ← Mem[i + Número do Grupo * 0120,h] 
-        lw $r6, grupo*0x0120+6($r31)              ;		r6 ← Mem[i + Número do Grupo * 0120,h] 
-        
-
-
-        add $r10, $r2, $r3                     ;		r10 ← r2 + r3 = 5
-        add $r11, $r4, $r5                     ;		r11 ← r4 + r5 = 9
-        add $r12, $r2, $r5                     ;		r12 ← r2 + r5 = 7
-        
-
-
-        
-        sw $r10, grupo*0x0120+1021($r31)         ;	MemDados[antepenultima posição RAM]   [r10]  
-        sw $r11, grupo*0x0120+1022($r31)         ;	MemDados[penultima posição RAM]       [r11]
-        sw $r12, grupo*0x0120+1023($r31)         ;	MemDados[ultima posição RAM]          [r12]
-
-        mul $r13, $r10, $r11;
-
-
-
-        lw $r7,  grupo*0x0120+1021($r31)         ;  r7 ←  MemDados[antepenultima posição RAM]
-        lw $r8,  grupo*0x0120+1022($r31)         ;  r8 ←  MemDados[penultima posição RAM]
-        lw $r9,  grupo*0x0120+1023($r31)         ;  r9 ←  MemDados[ultima posição RAM]
+                                                ;		Code presents data hazzard
+Main:	ori $r30, 32($r0)                   	;		r30 ← 32 (vector size)							
+		ori $r10, 0($r0)                        ;		r10 ← 0	
+		ori $r31, 0($r0)                        ;		r31	← 0	(auxiliary counter to sweep vector)	
+        add $r18, $r30, $r0                 	;       only to verify if ori $r30, 32($r0) works
+Loop:	lw $r1, grupo*0x0125($r31)          	;		r1 ← Mem[i + Número do Grupo * 0250h] 
+		addi $r31, 1($r31)                      ;		i = i + 1  
+		add $r10, $r1, $r10                     ;		r10 ← r1 + r10 (cumulative sum of nth vector value)
+		bne $r31, $r30, Loop                    ;   	if r31 != r30 then PC ← Loop Address
+		sw $r10, grupo*0x0125+1023($r15)        ;		MemDados[ultima posição RAM] ←  [r10]
+												;
+												;		Code solves data hazzard by adding bubbles
+		ori $r31, 0($r0)                        ;		r10 ← 0	
+		ori $r10, 0($r0)                        ;		r31	← 0	(auxiliary counter to sweep vector)	
+        ori $r13, 0x00FF($r0)               	;       r13 ← 0x00FF (constant to multiply vector sum)
+Loop1: 	lw $r1, grupo*0x0125($r31)              ;       r1 ← Mem[i + Número do Grupo * 0250h] 
+		addi $r31, 1($r31)                      ;		i = i + 1  
+		                                        ;		NOP as bubble
+		                                        ;		NOP as bubble
+		add $r10, $r1, $r10                     ;		r10 ← r1 + r10 (cumulative sum of nth vector value)
+                                                ;		NOP as bubble
+		                                        ;		NOP as bubble
+		bne $r31, $r30, Loop1                   ;		if r31 != r30 then PC ← Loop1 Address
+       	mul $r20, $r10, $r13                    ;      	r20 ← sum * 0x00FF
+                                                ;       NOP as bubble
+                                                ;      	NOP as bubble
+		sw $r20, grupo*0x0125+1023($r15)        ;		MemDados[ultima posição RAM] ←  [r10]
+		                                        ;
+		jmp Main                                ;		PC ← Main Address
